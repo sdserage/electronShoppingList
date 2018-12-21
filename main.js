24fc58781476f2703af9f22cfff5893dc495818e
sdserage <xttocsurge51@gmail.com>
2018-12-20 17:09:49 -0700

    Began giving menu functionality

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const electron = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
-
 let mainWindow;
+let addWindow;
 
 //  Listen for app to be ready
 app.on('ready', () => {
@@ -16,4 +15,52 @@ app.on('ready', () => {
     protocol: 'file:',
     slashes: true,
   }));
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Handle create add window
+function createAddWindow() {
+  // Create new window
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add Shopping List Item',
+  });
+  // Load html into window
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
+}
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item',
+        click: () => {
+          createAddWindow();
+        },
+      },
+      {
+        label: 'Clear Items',
+      },
+      {
+        label: 'Quit',
+        // How to write behavior of "role: 'quit'"
+        // accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        // click: () => {
+        //   app.quit();
+        // },
+        role: 'quit',
+      },
+    ],
+  },
+];
